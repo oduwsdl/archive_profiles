b7d1a0dc2073bbd00fb402d8bb4675c27391566a
Sawood Alam <ibnesayeed@gmail.com>
2014-12-08 11:56:36 -0500

    Writing to GitHub, Config file and some refactoring

--- src/profiler.py
+++ src/profiler.py
@@ -16,22 +16,34 @@ import tldextract
 import pprint
 import json
 import time
+import requests
+import ConfigParser
 
 class Profile(object):
     """Basic archive profile to be evolved by the profiler."""
 
     def __init__(self, name="", description="", homepage="", accesspoint="", memento_compliance="", timegate="", timemap="", established="", profile_updated="", **kwargs):
         """Initialize a basic archive profile object."""
-        self.name = name
-        self.description = description
-        self.homepage = homepage
-        self.accesspoint = accesspoint
-        self.memento_compliance = memento_compliance
-        self.timegate = timegate
-        self.timemap = timemap
-        self.established = established
-        self.profile_updated = profile_updated
-        self.__dict__.update(kwargs)
+        self.about = {
+            "name": name,
+            "description": description,
+            "homepage": homepage,
+            "accesspoint": accesspoint,
+            "memento_compliance": memento_compliance,
+            "timegate": timegate,
+            "timemap": timemap,
+            "established": established,
+            "profile_updated": profile_updated
+        }
+        self.__dict__["about"].update(kwargs)
+        self.stats = {}
+        setattr(self, "@context", "https://oduwsdl.github.io/contexts/archiveprofile.jsonld")
+        setattr(self, "@id", homepage)
+
+    def to_json(self):
+        """Serializes processed profile object in JSON format."""
+        print("Converting to JSNON...")
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4, separators=(',', ': '))
 
 class Profiler(object):
     """Profiling an archive using CDX files."""
@@ -39,35 +51,52 @@ class Profiler(object):
     def __init__(self, debug_level=3):
         """Initialize with a basic empty profile and cache PrettyPrinter object for debugging."""
         self.debug_level = debug_level
-        self.profile = {"tld": {}}
+        self.suburi = {}
         self.pp = pprint.PrettyPrinter(indent=2)
         self.debug("Profiling started...", 1)
 
     def build_profile(self, *cdxs):
         """Accepts a list of CDX file names/paths and calls CDX processor on them."""
         self.debug("Building profile from CDX files...", 1)
+        self.suburi["tld"] = {}
+        self.suburi["suburi"] = {}
         for cdx in cdxs[0]:
             self.process_cdx(cdx)
 
     def process_cdx(self, cdx):
         """Accepts a CDX file and processes it to extract neccessary information and builds a raw data structure."""
         self.debug("Processing CDX: " + cdx, 1)
-        profile = self.profile
+        suburi = self.suburi
         with open(cdx) as f:
             for line in f:
                 entry = self.parse_line(line)
                 if entry and entry.scheme.startswith("http"):
-                    try:
-                        profile["tld"][entry.tld]["domain"][entry.domain]["surt"][entry.surt] += 1
-                    except KeyError, e:
-                        if e.message == entry.tld:
-                            profile["tld"][entry.tld] = {"domain": {}}
-                            e.message = entry.domain
-                        if e.message == entry.domain:
-                            profile["tld"][entry.tld]["domain"][entry.domain] = {"surt": {}}
-                            e.message = entry.surt
-                        if e.message == entry.surt:
-                            profile["tld"][entry.tld]["domain"][entry.domain]["surt"][entry.surt] = 1
+                    self.flat_ds(entry)
+                    #self.nested_ds(entry)
+
+    def nested_ds(self, entry):
+        suburi = self.suburi
+        try:
+            suburi["tld"][entry.tld]["domain"][entry.domain]["surt"][entry.surt] += 1
+        except KeyError, e:
+            if e.message == entry.tld:
+                suburi["tld"][entry.tld] = {"domain": {}}
+                e.message = entry.domain
+            if e.message == entry.domain:
+                suburi["tld"][entry.tld]["domain"][entry.domain] = {"surt": {}}
+                e.message = entry.surt
+            if e.message == entry.surt:
+                suburi["tld"][entry.tld]["domain"][entry.domain]["surt"][entry.surt] = 1
+
+    def flat_ds(self, entry):
+        suburi = self.suburi
+        surtparts = entry.surt.split("?")[0].split("/")
+        uniqsurts = set([entry.tld, entry.domain, "/".join(surtparts[0:2]), "/".join(surtparts[0:3])])
+        for u in uniqsurts:
+            try:
+                suburi["suburi"][u] += 1
+            except KeyError, e:
+                suburi["suburi"][u] = 1
 
     def parse_line(self, line=""):
         """Parses single line of a CDX file and returns selected and derived attributes in a namedtuple."""
@@ -75,18 +104,18 @@ class Profiler(object):
         if len(segs) == 10:
             url = urlparse(segs[2])
             dom = tldextract.extract(segs[2])
-            Segments = namedtuple("Segments", "scheme, host, domain, tld, surt, mime")
-            return Segments(url.scheme, url.netloc, surt(dom.registered_domain), surt(dom.suffix), surt(segs[2]), segs[3])
+            Segments = namedtuple("Segments", "scheme, host, domain, tld, surt, uri, time, mime")
+            return Segments(url.scheme, url.netloc, surt(dom.registered_domain), surt(dom.suffix), surt(segs[2]), segs[2], segs[1], segs[3])
 
     def calculate_stats(self, flat=False):
         """Calculates statistics from the raw profile data structure and prepares the profile object for serialization."""
         self.debug("Calculating statistics...", 1)
-        profile = self.profile
+        suburi = self.suburi
         if flat:
-            profile["domain"] = {}
+            suburi["domain"] = {}
         psum = purir = 0
         pmin = pmax = 1
-        for t in profile["tld"].itervalues():
+        for t in suburi["tld"].itervalues():
             tsum = turir = 0
             tmin = tmax = 1
             for d in t["domain"].itervalues():
@@ -109,10 +138,10 @@ class Profiler(object):
             pmin = min(tmin, pmin)
             pmax = max(tmax, pmax)
             if flat:
-                profile["domain"].update(t["domain"])
+                suburi["domain"].update(t["domain"])
                 del t["domain"]
-        profile["urir"] = purir
-        profile["urim"] = {"total": psum, "min": pmin, "max": pmax}
+        suburi["urir"] = purir
+        suburi["urim"] = {"total": psum, "min": pmin, "max": pmax}
 
     def to_json(self, outfile=None):
         """Serializes processed profile object in JSON format."""
@@ -120,8 +149,8 @@ class Profiler(object):
         if outfile:
             self.debug("Writing output to " + outfile, 1)
             f = open(outfile, 'w')
-            json.dump(self.profile, f, sort_keys=True, indent=4, separators=(',', ': '))
-        self.debug(self.profile, 3)
+            json.dump(self.suburi, f, sort_keys=True, indent=4, separators=(',', ': '))
+        self.debug(self.suburi, 3)
 
     def debug(self, msg, level=0):
         """Utility method to log debug messages of various levels."""
@@ -130,13 +159,43 @@ class Profiler(object):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("To profile a CDX archive:")
+        print("\nTo profile a CDX archive:")
         print("  Single CDX file    :    profiler.py abc.cdx")
         print("  Multiple CDX files :    profiler.py abc.cdx def.cdx ...")
-        print("  Multiple CDX files :    profiler.py *.cdx abc/*.cdx ...")
+        print("  Multiple CDX files :    profiler.py *.cdx abc/*.cdx ...\n")
         sys.exit(0)
-    p = Profiler()
-    p.build_profile(sys.argv[1:])
-    p.calculate_stats(flat=True)
-    opf = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'json', "profile-"+time.strftime("%Y%m%d-%H%M%S")+".json")
-    p.to_json(opf)
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    config = ConfigParser.ConfigParser()
+    config.read(os.path.join(scriptdir, "config.ini"))
+    print(config.sections())
+    p = Profile(name=config.get("archive", "name"),
+                description=config.get("archive", "description"),
+                homepage=config.get("archive", "homepage"),
+                accesspoint=config.get("archive", "accesspoint"),
+                memento_compliance=config.get("archive", "memento_compliance"),
+                timegate=config.get("archive", "timegate"),
+                timemap=config.get("archive", "timemap"),
+                established=config.get("archive", "established"),
+                profile_updated=time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                mechanism="https://oduwsdl.github.io/terms/mechanism#cdx")
+    pr = Profiler()
+    pr.build_profile(sys.argv[1:])
+    #pr.calculate_stats(flat=True)
+    p.stats = pr.suburi
+    jsonstr = p.to_json()
+    opf = os.path.join(scriptdir, 'json', "profile-"+time.strftime("%Y%m%d-%H%M%S")+".json")
+    print("Writing output to " + opf)
+    f = open(opf, 'w')
+    f.write(jsonstr)
+    f.close()
+    gist = {
+        "description": "An archive profile created on "+time.strftime("%Y-%m-%d at %H:%M:%S")+".",
+        "public": True,
+        "files": {
+            "profile-"+time.strftime("%Y%m%d-%H%M%S")+".json": {
+                "content": jsonstr
+            }
+        }
+    }
+    r = requests.post(config.get("github", "endpoint"), data=json.dumps(gist), auth=(config.get("github", "user"), config.get("github", "token")))
+    print("Writing to GitHub: " + str(r.status_code))
